3577ae996d0f37f58e27bc76afb56df1c460a496
junho526 <jungjunho0526@naver.com>
2025-08-08 17:31:41 +0900

    feat: init Spring Boot (su.kdt) + Room API skeleton

---
+++ src/main/java/su/kdt/minigame/MiniGameServiceApplication.java
@@ -0,0 +1,14 @@
+package su.kdt.minigame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniGameServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniGameServiceApplication.class, args);
+    }
+
+}
+
